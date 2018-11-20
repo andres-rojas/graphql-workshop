25003e1fa077e059dbe922711e24aa1207fab730
Byron McMullen <byronsm@gmail.com>
2018-11-19 19:12:29 -0800

    apollo-client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Monsters from "./Monsters";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello World</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React in seconds
-          </a>
-        </header>
+        <Monsters />
       </div>
     );
   }
