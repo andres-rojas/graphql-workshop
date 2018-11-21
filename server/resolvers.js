6e129fa5e91b5d6e3d0d47cedbdf6562fcef05b5
Andres Rojas <andres@conpat.io>
2018-11-20 16:20:04 -0800

    Added examples from Byron's presentation

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,2 +1,15 @@
 // A map of functions which return data for the schema.
-module.exports = {};
+module.exports = {
+  Query: {
+    hello: () => "world",
+    monsters: (_parent, _args, context) =>
+      context.api.get('/monsters').then(r => r.data)
+  },
+  Mutation: {
+    createMonster: (_parent, args, context) =>
+      context.api.post('/monsters', args.monster).then(r => r.data)
+  },
+  Monster: {
+    hp: monster => parseInt(monster.hp, 10)
+  }
+};
